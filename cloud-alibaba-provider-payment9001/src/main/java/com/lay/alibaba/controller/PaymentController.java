4e85fe1fedeb362cb31baa242563b3906aa8965f
TheLay <yuelei980526@163.com>
2024-03-17 19:11:00 +0800

    ceshi

--- cloud-alibaba-provider-payment9001/src/main/java/com/lay/alibaba/controller/PaymentController.java
+++ cloud-alibaba-provider-payment9001/src/main/java/com/lay/alibaba/controller/PaymentController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping(value = "/payment")
 public class PaymentController {
@@ -14,6 +17,13 @@ public class PaymentController {
 
     @GetMapping(value = "/getServerPort")
     public String getServerPort() {
-        return "nacos registry serverPort :" + serverPort;
+        try{
+            InetAddress localhost = InetAddress.getLocalHost();
+            String ip = localhost.getHostAddress();
+            return "nacos registry :" + ip + ":" + serverPort;
+        }catch (UnknownHostException r) {
+            r.printStackTrace();
+        }
+        return "";
     }
 }
